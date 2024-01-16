135fa2931849eea04fc485ec7de6cd7968ddd184
Pablo Pelardas <pablo.pelardas@gmail.com>
2024-01-15 21:41:32 -0300

    client side session

--- 04-teslo-shop/src/components/index.ts
+++ 04-teslo-shop/src/components/index.ts
@@ -1,3 +1,4 @@
+export { Provider } from "./provider/Provider";
 export { Footer } from "./ui/footer/Footer";
 export { PageNotFound } from "./ui/not-found/PageNotFound";
 export { Pagination } from "./ui/pagination/Pagination";
--- 04-teslo-shop/src/components/ui/sidebar/Sidebar.tsx
+++ 04-teslo-shop/src/components/ui/sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@
 import { logout } from "@/actions";
 import { useUIStore } from "@/store";
 import clsx from "clsx";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import {
   IoCloseOutline,
@@ -17,6 +18,10 @@ import {
 export const Sidebar = () => {
   const isSideMenuOpen = useUIStore((state) => state.isSideMenuOpen);
   const closeSideMenu = useUIStore((state) => state.closeSideMenu);
+
+  const { data: session } = useSession();
+  console.log(session);
+
   return (
     <div>
       {isSideMenuOpen && (
